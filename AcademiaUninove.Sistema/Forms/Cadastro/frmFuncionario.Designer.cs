ef71d9dfbc4f29b692659e689eaf431f63a0ab9c
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2015-12-07 00:07:15 -0200

    teste

--- AcademiaUninove.Sistema/Forms/Cadastro/frmFuncionario.Designer.cs
+++ AcademiaUninove.Sistema/Forms/Cadastro/frmFuncionario.Designer.cs
@@ -236,7 +236,7 @@
             this.Controls.Add(this.btnSalvar);
             this.Controls.Add(this.btnCancelar);
             this.Name = "frmFuncionario";
-            this.Text = "Cadastro de Funcionários";
+            this.Text = "Cadastro de Funcionário";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
