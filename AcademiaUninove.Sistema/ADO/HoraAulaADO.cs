b283c14942c8fd3c35880bad7dc10474323d5aa6
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2015-12-09 13:33:52 -0200

    Terminado a regra de negocio e banco de HoraAula

--- AcademiaUninove.Sistema/ADO/HoraAulaADO.cs
+++ AcademiaUninove.Sistema/ADO/HoraAulaADO.cs
@@ -6,13 +6,117 @@ using System.Threading.Tasks;
 using System.Data;
 using AcademiaUninove.Sistema.Helpers;
 
+
 namespace AcademiaUninove.Sistema.ADO
 {
-    public class HoraAulaADO
+    public class HoraAulaADO : ADOBase
     {
         #region Variáveis globais
         string conn = string.Empty;
         ADOBase objConexao = new ADOBase();
         #endregion
+
+        public DataTable ConsultaHoraAula() {
+            bool bRet = false;
+            DataTable dtHoraAula = new DataTable();
+            string query = AcademiaUninove.Sistema.Queries.Read.QueriesConsulta.qConsultaHoraAula;
+
+            try
+            {
+                conn = ADOBase.ObterConexao();
+                dtHoraAula = RealizaPesquisaSQL(query, conn);
+            }
+            catch (Exception)
+            {
+                dtHoraAula = null;
+                throw;
+            }
+            return dtHoraAula;
+        }
+
+        public DataTable ConsultaHoraAula(int codigo)
+        {
+            bool bRet = false;
+            DataTable dtHoraAula = new DataTable();
+            string query = string.Format( AcademiaUninove.Sistema.Queries.Read.QueriesConsulta.qConsultaHoraAula, codigo);
+
+            try
+            {
+                conn = ADOBase.ObterConexao();
+                dtHoraAula = RealizaPesquisaSQL(query, conn);
+            }
+            catch (Exception)
+            {
+                dtHoraAula = null;
+                throw;
+            }
+            return dtHoraAula;
+        }
+
+        /// <summary>
+        /// Insere Hora Aula
+        /// </summary>
+        /// <param name="codigo"></param>
+        /// <param name="codHorario"></param>
+        /// <param name="codAula"></param>
+        /// <returns></returns>
+        public bool InsereHoraAula(int codigo, int codHorario, int codAula)
+        {
+            string query = string.Format(AcademiaUninove.Sistema.Queries.Insert.QueriesInsert.qInsertHoraAula, codigo, codHorario, codAula);
+            conn = ADOBase.ObterConexao();
+
+            try
+            {
+                RealizaComandoSQL(query, conn);
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+                return false;
+
+            }
+
+            return false;
+        }
+
+        public bool AtualizaHoraAula(int codigo, int codHorario, int codAula)
+        {
+            string query = string.Format(AcademiaUninove.Sistema.Queries.Update.QueriesUpdate.qUpdateHoraAula,
+                                        codigo,
+                                        codHorario,
+                                        codAula);
+            conn = ADOBase.ObterConexao();
+
+            try
+            {
+                RealizaComandoSQL(query, conn);
+                return true;
+            }
+            catch (Exception)
+            {
+                
+                throw;
+                return false;
+            }
+        }
+
+        public bool ExcluiHoraAula(int codigo)
+        {
+            string query = string.Format(AcademiaUninove.Sistema.Queries.Delete.QueriesDelete.qDeleteHoraAula, codigo);
+            conn = ADOBase.ObterConexao();
+
+            try
+            {
+                RealizaComandoSQL(query, conn);
+                return true;
+            }
+            catch (Exception)
+            {
+                
+                throw;
+                return false;
+            }
+        }
     }
 }
--- AcademiaUninove.Sistema/Negocio/HoraAulaBO.cs
+++ AcademiaUninove.Sistema/Negocio/HoraAulaBO.cs
@@ -3,14 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using AcademiaUninove.Sistema.ADO;
 
 namespace AcademiaUninove.Sistema.Negocio
 {
-    class HoraAulaBO
+    public class HoraAulaBO
     {
-        internal bool ExcluiHorario(int _codHorario)
+        HoraAulaADO objHoraAulaADO = new HoraAulaADO();
+
+        public DataTable ObterHoraAula()
         {
-            throw new NotImplementedException();
+            HoraAulaADO objHoraAulaADO = new HoraAulaADO();
+            DataTable dtHoraAula = new DataTable();
+
+            dtHoraAula = objHoraAulaADO.ConsultaHoraAula();
+            return dtHoraAula;
         }
+
+        public DataTable ObterHoraAula(int codigo)
+        {
+            HoraAulaADO objHoraAulaADO = new HoraAulaADO();
+            DataTable dtHoraAula = new DataTable();
+
+            dtHoraAula = objHoraAulaADO.ConsultaHoraAula(codigo);
+            return dtHoraAula;
+        }
+
+        public bool InsereHoraAula(int codigo, int coHorario, int codAula)
+        {
+            bool bRet = false;
+            bRet = objHoraAulaADO.InsereHoraAula(codigo,
+                                                 coHorario,
+                                                 codAula);
+            return bRet;
+        }
+
+        public bool AtualizaHoraAula(int codigo, int coHorario, int codAula)
+        {
+            bool bRet = false;
+            bRet = objHoraAulaADO.AtualizaHoraAula(codigo,
+                                                 coHorario,
+                                                 codAula);
+            return bRet;
+        }
+
+        public bool ExcluiHoraAula(int codigo)
+        {
+            bool bRet = false;
+            bRet = objHoraAulaADO.ExcluiHoraAula(codigo);
+            return bRet;
+        }
+       
     }
 }
