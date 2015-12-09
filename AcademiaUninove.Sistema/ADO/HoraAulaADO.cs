b7002e1c7722667d98388d4cfcf6c682acce682c
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2015-12-09 13:01:28 -0200

    Adicionando classe HoraAulaADO

---
+++ AcademiaUninove.Sistema/ADO/HoraAulaADO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using AcademiaUninove.Sistema.Helpers;
+
+namespace AcademiaUninove.Sistema.ADO
+{
+    public class HoraAulaADO
+    {
+        #region Variáveis globais
+        string conn = string.Empty;
+        ADOBase objConexao = new ADOBase();
+        #endregion
+    }
+}
